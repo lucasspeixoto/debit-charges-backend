60cbc35f765e8acb5b478b17b8831213de03f96a
Lucas Peixoto <lspeixotodev@gmail.com>
2024-03-25 20:30:48 -0300

    Starting repo

---
+++ src/main/java/com/lspeixotodev/debitcharges/DebitChargesApplication.java
@@ -0,0 +1,13 @@
+package com.lspeixotodev.debitcharges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitChargesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebitChargesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lspeixotodev/debitcharges/DebitChargesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lspeixotodev.debitcharges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitChargesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
