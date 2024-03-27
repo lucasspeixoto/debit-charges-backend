680eb666393f8f1c4f9682f1b65587105a244c15
Lucas Peixoto Fernandes <lspeixotodev@gmail.com>
2024-03-26 22:06:02 -0300

    Finish backend

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-Debit Charges Backend
+<h1 align="center">
+   Debit Charges - Backend
+</h1>
+
+## 💻 Repositório
+
+Projeto [Backend](https://github.com/lucasspeixoto/debit-charges-backend)
+## ✨ Tecnologias
+
+O projeto foi desenvolvido com as seguintes tecnologias:
+
+- [Sprint Boot](https://spring.io/projects/spring-boot)
+- [Maven](https://maven.apache.org/)
+- [MySQL](https://www.mysql.com/)
+- [Docker](https://www.docker.com/)
+
+## 🚀 Inicialização com Docker Compose
+
+```bash
+# Clone o repositório e entre na pasta do projeto
+$ git clone https://github.com/lucasspeixoto/debit-charges-backend && cd debit-charges-backend
+
+# Execute a aplicação em uma máquina com docker
+$ docker-compose up --build
+
+# O backend iniciará na porta 6060
+```
+
+## 📝 Licença
+
+Este projeto está licenciado sob a Licença MIT. Veja o arquivo [LICENSE](https://opensource.org/licenses/MIT) para obter mais detalhes.
+
---
+++ src/main/java/com/lspeixotodev/debitcharges/configs/WebConfig.java
@@ -0,0 +1,46 @@
+package com.lspeixotodev.debitcharges.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Value("${cors.allowed-methods:default}")
+    private String allowedMethods = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        var allowedOrigins = corsOriginPatterns.split(",");
+
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
+        
+        WebMvcConfigurer.super.configureContentNegotiation(configurer);
+    }
+}
